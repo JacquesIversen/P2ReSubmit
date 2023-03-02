cb9b544eff696ae766dde9bf0829abefc0103dff
JacquesIversen <jackiracki1@gmail.com>
2023-03-02 13:44:57 +0000

    Java created and tested. Questions added

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const question = document.getElementsByClassName("question")
+const choice = document.getElementById("choice")
+
+let answers = []
+let questions = [
+  {
+    question: "In what team did lewis hamilton optain his first F1 World championship?",
+    choice1: "Mclaren",
+    choice2: "Mercedes AMG",
+    choice3: "Ferrari",
+    choice4: "Force India",
+    answer: 1
+  },
+  {
+    question:
+      "sebastian vettel is a beloved character, but when did he win his first f1 race?",
+    choice1: "Italian GP 2008",
+    choice2: "Abu Dhabi GP 2011",
+    choice3: "Canada GP 2009",
+    choice4: "US GP 2009",
+    answer: 1
+  },
+  {
+    question: "nico rosberg is known for being the only racedriver to ever beat lewis hamilton in equal machinery, but who replaced him?",
+    choice1: "Robert Kubica",
+    choice2: "Kevin Magnussen",
+    choice3: "Valtteri Bottas",
+    choice4: "George Russell",
+    answer: 3
+  }];
+
+function showQuestions(questions) { //Generates questions.
+    var output = [];
+    var answers;
+
+    for(var i = 0; i < questions.length; i++) {
+
+    }
+
+
+}
+
+  
+
+console.log("Java er en lille so")
