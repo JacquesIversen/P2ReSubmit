ae9bea303bdadc88f49c826a54ad07eafe1ee94c
JacquesIversen <jackiracki1@gmail.com>
2023-03-03 20:13:45 +0000

    Added get options loop.

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,24 +30,48 @@ const questions = [
 
 
        // Getting the question
-    const question = document.getElementById("question");
-
+    
+    const question = questions[0]//document.getElementById("question");
+    var id = 0 //Math.floor(Math.random() * questions.length);
     console.log("dav")
     // Setting the question text
-    question.innerText = questions[id].q;
-    const option1 = document.getElementById('option1');
+    const questionText = questions[id].q;
+    const questionTextDiv = document.getElementById("questionText");
+    questionTextDiv.innerText = questionText;
+    const questionsAnswers = questions[id].answer
+    console.log(questionsAnswers)
+
+    for (let i=0; i<questionsAnswers.length; i++) {
+        // const answerOption = questions[i].answer
+        console.log(questionsAnswers[i])
+        const option = document.getElementById('option'+i);
+        option.innerText = questionsAnswers[i].option
+        option.value = questionsAnswers[i].isCorrect
+    }
+
+
+
+    //const option1 = document.getElementById('option1');
     const option2 = document.getElementById('option2');
     const option3 = document.getElementById('option3');
-    option1.innerText = questions[id].answer[0].text;
-    option2.innerText = questions[id].answer[1].text;
-    option3.innerText = questions[id].answer[2].text;
-    option1.value = questions[id].answer[0].isCorrect;
-    option2.value = questions[id].answer[1].isCorrect;
-    option3.value = questions[id].answer[2].isCorrect;
+    //option1.innerText = questions[id].answer[0].option;
+    //option2.innerText = questions[id].answer[1].option;
+    //option3.innerText = questions[id].answer[2].option;
+    //option1.value = questions[id].answer[0].isCorrect;
+    //option2.value = questions[id].answer[1].isCorrect;
+    //option3.value = questions[id].answer[2].isCorrect;
     console.log("dav igen")
-}
+
 startGame = () => {
     question();
   };
+
+onclickAnswer = (id) => {
+    console.log(id)
+}
+
+submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+}
 
   startGame();
